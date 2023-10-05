bce59036d2fa8222393462044c8454495362a67f
shreyu2000 <shreyamdake2000@gmail.com>
2023-10-05 22:39:16 +0530

    second commit

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
+let display = document.getElementById('display');
+let buttons = document.querySelectorAll('button')
 
+let string = " ";
 
-let string = "";
+let arr = Array.from(buttons);
 
+arr.forEach(button => {
+    button.addEventListener('click', (e)=>{
+        if(e.target.innerHTML == '='){
+            string =eval(string);
+            display.value = string;
+        }
+        else if(e.target.innerHTML == 'Reset'){
+                string = " ";
+                display.value ="0";
+        }
+        else if(e.target.innerHTML == 'Del'){
+            string = string.slice(0, -1);    //substring(0, string.length-1);
+            display.value = string;
+        }
+        else{
+            string +=e.target.innerHTML;
+            display.value =string;
+        }
+    });
+})
